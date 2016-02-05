1889cab428bf14e60e02339557e4f6d6179f0657
Bastian Turcs <basti@h-26.de>
2016-02-05 23:49:22 +0100

    extended parser

---
+++ paspaspas/Internal/Parser/Syntax/ExpressionBase.cs
@@ -0,0 +1,17 @@
+﻿using PasPasPas.Api;
+
+namespace PasPasPas.Internal.Parser.Syntax {
+
+    /// <summary>
+    ///     base class for expressions
+    /// </summary>
+    public abstract class ExpressionBase : SyntaxPartBase {
+
+        /// <summary>
+        ///     create a new syntax tree element
+        /// </summary>
+        /// <param name="informationProvider">current parser state</param>
+        protected ExpressionBase(IParserInformationProvider informationProvider) : base(informationProvider) { }
+
+    }
+}
---
+++ paspaspas/Internal/Parser/Syntax/SimplExpr.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using PasPasPas.Api;
+
+namespace PasPasPas.Internal.Parser.Syntax {
+
+    /// <summary>
+    ///     simple expression
+    /// </summary>
+    public class SimplExpr : SyntaxPartBase {
+
+        /// <summary>
+        ///     create a new syntax tree element
+        /// </summary>
+        /// <param name="informationProvider">current parser state</param>
+        public SimplExpr(IParserInformationProvider informationProvider) : base(informationProvider) { }
+
+        /// <summary>
+        ///     format expression
+        /// </summary>
+        /// <param name="result"></param>
+        public override void ToFormatter(PascalFormatter result) {
+            throw new NotImplementedException();
+        }
+    }
+}
