c22c90a39a650c5c8b1d7638c176828fb8d09929
prjm <prjm@users.noreply.github.com>
2018-08-10 00:43:55 +0200

    syntax tree changes

---
+++ PasPasPas.Parsing/src/SyntaxTree/Standard/SetSectn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Immutable;
+using PasPasPas.Parsing.SyntaxTree.Utils;
+using PasPasPas.Parsing.SyntaxTree.Visitors;
+
+namespace PasPasPas.Parsing.SyntaxTree.Standard {
+
+    /// <summary>
+    ///     set expression
+    /// </summary>
+    public class SetSection : VariableLengthSyntaxTreeBase<SetSectnPart> {
+
+        /// <summary>
+        ///     create a new set section
+        /// </summary>
+        /// <param name="openBraces"></param>
+        /// <param name="items"></param>
+        /// <param name="closeBraces"></param>
+        public SetSection(Terminal openBraces, ImmutableArray<SetSectnPart> items, Terminal closeBraces) : base(items) {
+            OpenBraces = openBraces;
+            CloseBraces = closeBraces;
+        }
+
+        /// <summary>
+        ///     open braces
+        /// </summary>
+        public Terminal CloseBraces { get; }
+
+        /// <summary>
+        ///     close braces
+        /// </summary>
+        public Terminal OpenBraces { get; }
+
+        /// <summary>
+        ///     accept visitor
+        /// </summary>
+        /// <param name="visitor">visitor</param>
+        public override void Accept(IStartEndVisitor visitor) {
+            visitor.StartVisit(this);
+            AcceptPart(this, OpenBraces, visitor);
+            AcceptPart(this, visitor);
+            AcceptPart(this, CloseBraces, visitor);
+            visitor.EndVisit(this);
+        }
+
+        /// <summary>
+        ///     symbol length
+        /// </summary>
+        public override int Length
+            => OpenBraces.GetSymbolLength() +
+                ItemLength +
+                CloseBraces.GetSymbolLength();
+
+
+    }
+}
